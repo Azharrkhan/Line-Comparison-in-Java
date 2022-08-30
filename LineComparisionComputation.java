bfd6ab5e45cbef09988245192f0a2237d17e5e97
Azhar <azharumi786@gmail.com>
2022-08-30 14:26:45 +0530

    added line

--- LineComparisionComputation.java
+++ LineComparisionComputation.java
@@ -2,12 +2,19 @@ package com.bl.linecomparison;
 
 import java.util.Scanner;
 
+import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
+
 public class LineComparisionComputation {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Program");
-		System.out.println("Enter x1, y1");
+		
 		Scanner sc = new Scanner(System.in);
+		
+//Let consider the end points of the first line are (x1 ,y1) and (x2, y2)
+		
+		System.out.println("Enter x1, y1");
+		
 		int x1 = sc.nextInt();
 		int y1 = sc.nextInt();
 		
@@ -20,8 +27,30 @@ public class LineComparisionComputation {
 		
 		System.out.println(x2 + "," + y2);
 		
-		int length = (int) Math.sqrt(Math.pow((double) (x2-x1),2) + Math.pow((double) (y2-y1),2));
+//Let consider the end points of the first line are (x3 ,y3) and (x4, y4)
 		
-		System.out.println("Length is : "  + length);
+		System.out.println("Enter x3, y3");
+		
+		int x3 = sc.nextInt();
+		int y3 = sc.nextInt();
+		
+		System.out.println(x3 + "," + y3);
+		
+		System.out.println("Enter x4, y4");
+		
+		int x4 = sc.nextInt();
+		int y4 = sc.nextInt();
+		
+		System.out.println(x4 + "," + y4);
+		
+		int length1 = (int) Math.sqrt(Math.pow((double) (x2-x1),2) + Math.pow((double) (y2-y1),2));
+		
+		System.out.println("Length of first line is : "  + length1);
+		
+		int length2 = (int) Math.sqrt(Math.pow((double) (x4-x3),2) + Math.pow((double) (y4-y3),2));
+		
+		System.out.println("Length of second line is : "  + length2);
+		
+		System.out.println("length1 is equal to length2 = " + (length1 == length2));
 	}
 }
