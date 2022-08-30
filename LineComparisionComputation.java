3eba9171c45dcd8e71bddbde889b7b88465c156b
Azhar <azharumi786@gmail.com>
2022-08-30 13:17:09 +0530

    added Line

---
+++ LineComparisionComputation.java
@@ -0,0 +1,11 @@
+package com.bl.linecomparison;
+
+import java.util.Scanner;
+
+public class LineComparisionComputation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Program");
+		
+	}
+}
