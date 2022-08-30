94b7ea5d1a859012721bdbf0c14ca56b481385f4
Azhar <azharumi786@gmail.com>
2022-08-30 13:03:25 +0530

    added UC1

---
+++ UC1.java
@@ -0,0 +1,16 @@
+package com.bl.linecomparison;
+
+import java.util.Scanner;
+
+public class UC1 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Program");
+		
+	    Scanner sc = new Scanner(System.in);
+	    
+		
+		}
+	}
+
+
